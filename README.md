ba451c5af5b78e1881bd93ca251c2ff8e7269849
skywalker139 <akshatnamdeo2412@gmail.com>
2024-06-20 19:04:10 +0530

    Add .gitattributes to manage line endings

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Social-media
 Aries project: Creating a social media website
+I've named it WatchTower
 
 Akshat Namdeo 
 BTech 1Y EE
 23115008
+
+python -m pip install -r requirements.txt
+https://sqlitebrowser.org/dl/
 
 Social media website using html css and flask.
 Sources: gfg, mdn documentations, youtube
--- app.py
+++ app.py
@@ -1,17 +1,132 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_bcrypt import Bcrypt
+from flask_login import login_user, current_user, logout_user, login_required, LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ae2f15a133955e13ad6d874828ffc119'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-posts=[]
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+#models
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+    
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+posts=[
+    {
+        "id": 1,
+        "body": "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip. Sea lettuce lettuce water chestnut eggplant winter purslane fennel azuki bean earthnut pea sierra leone bologi leek soko chicory celtuce parsley jícama salsify.",
+        "title": "The Life of Cactus",
+        "subtitle": "Who knew that cacti lived such interesting lives."
+    },
+    {
+        "id": 2,
+        "body": "Chase ball of string eat plants, meow, and throw up because I ate plants going to catch the red dot today going to catch the red dot today. I could pee on this if I had the energy. Chew iPad power cord steal the warm chair right after you get up for purr for no reason leave hair everywhere, decide to want nothing to do with my owner today.",
+        "title": "Top 15 Things to do When You are Bored",
+        "subtitle": "Are you bored? Don't know what to do? Try these top 15 activities."
+    },
+    {
+        "id": 3,
+        "body": "Cupcake ipsum dolor. Sit amet marshmallow topping cheesecake muffin. Halvah croissant candy canes bonbon candy. Apple pie jelly beans topping carrot cake danish tart cake cheesecake. Muffin danish chocolate soufflé pastry icing bonbon oat cake. Powder cake jujubes oat cake. Lemon drops tootsie roll marshmallow halvah carrot cake.",
+        "title": "Introduction to Intermittent Fasting",
+        "subtitle": "Learn about the newest health craze."
+    }
+]
+
+
+#routes
 
 @app.route("/")
 @app.route("/home")
 def homepage():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
-@app.route("/login")
-def homepage():
-    return render_template('login.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', form=form)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+    # form = RegistrationForm()
+    # if form.validate_on_submit():
+    #     flash(f'Account created for {form.username.data}!', 'success')
+    #     return redirect(url_for('home'))
+    # return render_template('register.html', form=form)
+    # # if request.method == "POST":
+    # #     new_user = User(
+    # #         username=request.form["username"],
+    # #         email=request.form["email"],
+    # #         password=request.form["password"]
+    # #     )
+    # #     db.session.add(new_user)
+    # #     db.session.commit()
+    # #     return redirect(url_for('home'))
+
+with app.app_context():
+    db.create_all()
 
 if  __name__ == "__main__":
     app.run(debug=True)
